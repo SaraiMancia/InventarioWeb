1dee565b18db15cd36c9e99b270cde3992559105
Melani05 <melanibeltran68@gmail.com>
2022-06-26 22:59:25 -0300

    ......

--- src/java/Controller/ProductosS.java
+++ src/java/Controller/ProductosS.java
@@ -1,83 +1,92 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Controller;
 
+import DAO.ProductoDAO;
+import DAO.ProductoDAOImplementar;
+import Model.Producto;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author USER
- */
 public class ProductosS extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ProductosS</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ProductosS at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   protected void listaProducto(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        ProductoDAO producto = new ProductoDAOImplementar();
+        HttpSession sesion = request.getSession(true);
+        sesion.setAttribute("lista", Producto.Listar());
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Vistas-Producto/listarProducto.jsp");
+        dispatcher.forward(request, response);
+    } 
+    
+   
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+      //this.listaCategorias(request, response);
+      
+         String estado = request.getParameter("opcion");
+         String id_producto = request.getParameter("id");
+         String nom_producto = request.getParameter("nombre");
+         String categoria_id = request.getParameter("categoria");
+         String stock = request.getParameter("stock");
+         String precio = request.getParameter("precio");
+         String unidadMedida = request.getParameter("unidad de medida");
+         String estado_p = request.getParameter("estado producto");
+    
+
+    
+    if(estado.equals("listar")){
+             this.listaProducto(request, response);
+         }else if(estado.equals("editar")){
+             System.out.println("Editando productos....");
+             RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Vistas-Producto/editarProductos.jsp?id="+id_producto+"&&nombre="+nom_producto+"&&estado="+estado_p);
+             dispatcher.forward(request, response);
+         }else if(estado.equals("eliminar")){
+             System.out.println("Baja de productos...");
+             RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Vistas-Producto/eliminarProductos.jsp?id="+id_producto+"&&nombre="+nom_producto);
+             dispatcher.forward(request, response);
+         }else if(estado.equals("crear")){
+             System.out.println("Crear productos...");
+             String pagina = "/Vistas-Producto/crearProducto.jsp";
+             RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(pagina);
+             dispatcher.forward(request, response);
+         }else{
+             
+         }
+         
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+       Producto producto = new Producto();
+       
+        producto.setId_producto(Integer.parseInt(request.getParameter("id_producto")));
+        producto.setNom_producto(request.getParameter("txtNomProducto"));
+        producto.setEstado(Integer.parseInt(request.getParameter("txtEstadoProducto")));
+
+        ProductoDAO guardarProducto = new ProductoDAOImplementar();
+        guardarProducto.guardarProd(producto);
+        this.listaProducto(request, response);
+        
+        
+         
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
