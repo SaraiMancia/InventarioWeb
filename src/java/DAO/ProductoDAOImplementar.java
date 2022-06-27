0a19a4115f03235ea47897273b04b2080c5c9b30
SaraiMancia <manciabrego@gmail.com>
2022-06-27 17:32:06 -0600

    pro

--- src/java/DAO/ProductoDAOImplementar.java
+++ src/java/DAO/ProductoDAOImplementar.java
@@ -4,35 +4,36 @@ package DAO;
 
 import Factory.ConexionBD;
 import Factory.FactoryConexionBD;
+import Model.Categoria;
 import Model.Producto;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductoDAOImplementar implements ProductoDAO {
-     public ProductoDAOImplementar() {
-     
-    } 
+
+
+     public class ProductoDAOImplementar implements ProductoDAO {
+    
+     ConexionBD conn;
+
+    public ProductoDAOImplementar() {
+    
+    }
 
     @Override
     public List<Producto> Listar() {
-         this.conn = FactoryConexionBD.open(FactoryConexionBD.MySQL);
+      this.conn = FactoryConexionBD.open(FactoryConexionBD.MySQL);
         StringBuilder miSQL = new StringBuilder();
         miSQL.append("select * from tb_producto;");
         List<Producto> lista = new ArrayList<Producto>();
         try{
             ResultSet resultadoSQL = this.conn.consultaSQL(miSQL.toString());
             while(resultadoSQL.next()){
-                Producto producto = new Producto();
-                producto.setId_producto(resultadoSQL.getInt("id_producto));
-                producto.setNom_producto(resultadoSQL.getString("nom_producto"));
-                producto.setCategoria_id(resultadoSQL.getInt("categoria_id));"
-                producto.setStock(resultadoSQL.getfloat("stock));"
-                producto.setPrecio(resultadoSQL.getfloat("precio));"
-                producto.setUnidadMedida(resultadoSQL.getString("unidadMedida"));
-                producto.setEstado(resultadoSQL.getInt("estado"));
-                producto.setCategoria(resultadoSQL.getCategoria("categoria));
-                lista.add(producto);
+                Producto prodc = new Producto();
+                prodc.setId_producto(resultadoSQL.getInt("id_producto"));
+                prodc.setNom_producto(resultadoSQL.getString("nom_producto"));
+                prodc.setCategoria_id(resultadoSQL.getInt("categoria_id"));
+                lista.add(prodc);
             }
         }catch(Exception ex){
             ex.printStackTrace();
@@ -41,6 +42,7 @@ public class ProductoDAOImplementar implements ProductoDAO {
         }
         
         return lista;
+    
     }
 
     @Override
@@ -49,83 +51,18 @@ public class ProductoDAOImplementar implements ProductoDAO {
     }
 
     @Override
-    public Producto editarCat(int id_prod_edit) {
-        this.conn = FactoryConexionBD.open(FactoryConexionBD.MySQL);
-        Producto producto = new Producto();
-        StringBuilder miSQL = new StringBuilder();
-        miSQL.append("select * from tb_producto where id_producto = ").append(id_prod_edit);
-        try{
-            ResultSet resultadoSQL = this.conn.consultaSQL(miSQL.toString());
-            while(resultadoSQL.next()){
-               producto.setId_producto(resultadoSQL.getInt("id_producto"));
-                producto.setNom_producto(resultadoSQL.getString("nom_producto"));
-                producto.setCategoria_id(resultadoSQL.getInt("categoria_id));"
-                producto.setStock(resultadoSQL.getfloat("stock));"
-                producto.setPrecio(resultadoSQL.getfloat("precio));"
-                producto.setUnidadMedida(resultadoSQL.getString("unidadMedida"));
-                producto.setEstado(resultadoSQL.getInt("estado_p"));
-                producto.setCategoria(resultadoSQL.getCategoria("categoria));
-            }
-            
-        }catch(Exception e){
-            
-        }finally{
-            this.conn.cerrarConexion();
-        }
-        
-        return producto;
+    public Producto editarProd(int id_prod_edit) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public boolean guardarProd(Producto producto) {
-           this.conn = FactoryConexionBD.open(FactoryConexionBD.MySQL);
-        boolean guarda = false;
-        
-        try{
-            if(producto.getId_producto() == 0){
-                System.out.println("Guardar");
-                StringBuilder miSQL = new StringBuilder();
-                miSQL.append("INSERT INTO tb_producto(nom_producto, estado_producto) VALUES('");
-                miSQL.append(producto.getNom_producto() + "', ").append(producto.getEstado_producto());
-                miSQL.append(");");
-                this.conn.ejecutarSQL(miSQL.toString());
-            }else if(producto.getId_producto() > 0){
-                
-               System.out.println("Update");
-                StringBuilder miSQL = new StringBuilder();
-                miSQL.append("UPDATE tb_producto SET id_producto = ").append(producto.getId_producto());
-                miSQL.append(", nom_producto = '").append(producto.getNom_producto());
-                miSQL.append("', estado_producto = ").append(producto.getEstado_producto());
-                miSQL.append(" WHERE id_producto = ").append(producto.getId_producto()).append(";");
-                this.conn.ejecutarSQL(miSQL.toString());
-            }
-           guarda = true;
-        }catch(Exception e){
-            
-        }finally{
-            this.conn.cerrarConexion();
-        }
-        
-        return guarda;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean borrarCat(int id_prod_borrar) {
-           this.conn = FactoryConexionBD.open(FactoryConexionBD.MySQL);
-        boolean borra = false;
-        try{
-            StringBuilder miSQL = new StringBuilder();
-            miSQL.append("Delete from tb_producto where id_producto = ").append(id_prod_borrar);
-            this.conn.ejecutarSQL(miSQL.toString());
-            borra = true;
-        }catch(Exception e){
-            
-        }finally{
-            this.conn.cerrarConexion();
-        }
+    public boolean borrarProd(int id_prod_borrar) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+     }
         
-        return borra;
-    }
-    }
-    
-}
--- web/Vistas-Categorias/editarCategoria.jsp
+++ web/Vistas-Categorias/editarCategoria.jsp
@@ -16,6 +16,41 @@
     </head>
     <body class="m-0 row justify-content-center">
          <%@include file = "../WEB-INF/Vistas-Parciales/encabezado.jspf" %>
+             <center>
+    <body>
+        <%@include file = "../WEB-INF/Vistas-Parciales/encabezado.jspf" %>
+        
+     
+       <div class="card text-center" style="width: 18rem;">
+  <div class="card-body">
+  <h5 class="card-title"></h5>
+    <p class="card-text"> 
+ <h1 class='text-center'>Información de Categoria</h1>
+        <form action="modificarCategoria.do" method="post">
+        <div class="mb-3 row">
+            <label for="staticEmail" class="col-sm-2 col-form-label">ID Categoria:</label>
+            <div class="col-sm-10">
+              <input type="text" value='<%= id_cat %>' name="id1" id="id1" readonly class="form-control-plaintext" id="staticEmail" placeholder="ID Categoria">
+              <input type="hidden" value='<%= id_cat %>' name="id" id="id" readonly class="form-control-plaintext" id="staticEmail" >
+            </div>
+        </div>
+                   <td align="right"> Nombre: </td>                  
+                   <td>    <input type="text" value='<%= nombre_cat %>' name="nombre" id="nombre" class="form-control" placeholder="Nombre Categoria" required="true"></td>  <br><br>         
+                      <td align="right"> Estado: </td>                  
+                      <td>      <input type="text" value='<%= estado_cat %>' name="estado" id="estado" class="form-control" placeholder="Estado Categoria" required="true"> </td> <br><br>          
+                      
+ 
+          <div class="mb-3 row">
+          <button type="submit" name="send" class="btn btn-success">Actualizar Categoria</button>
+          </div>  
+       </form>  
+ 
+  </div>
+</div>
+  
+                <%@include file="../WEB-INF/Vistas-Parciales/pie.jspf" %>
+    </body>
+    </center> 
          <div class="col-auto bg-gray p-5 text-center">
         <h1 class='text-center'>Información de Categoria</h1>
         <form action="modificarCategoria.do" method="post">
