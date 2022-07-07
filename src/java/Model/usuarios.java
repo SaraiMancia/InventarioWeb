39fc6c392bfc5f7e7151fa78b649bad58c85992b
Rocio104 <rociominero07@gmail.com>
2022-07-07 03:00:12 -0600

    modelo usuario

---
+++ src/java/Model/usuarios.java
@@ -0,0 +1,138 @@
+package Model;
+
+public class usuarios {
+    int id;
+    String nombre;
+    String apellido;
+    String correo;
+    String usuario;
+    String clave;
+    int telefono;
+    int tipo;
+    int estado;
+    String pregunta;
+    String respuesta;
+    String fecha;
+
+    public usuarios() {
+    }
+
+    public usuarios(int id, String nombre, String apellido, String correo, String usuario, String clave, int telefono, int tipo, int estado, String pregunta, String respuesta, String fecha) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.usuario = usuario;
+        this.clave = clave;
+        this.telefono = telefono;
+        this.tipo = tipo;
+        this.estado = estado;
+        this.pregunta = pregunta;
+        this.respuesta = respuesta;
+        this.fecha = fecha;
+    }
+
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+    
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+    
+    
+}
